2c37eaac9b45bea7670217807109107b84cd1e9d
Mark Beck <>
2023-11-08 02:50:42 +0100

    added config update http command

--- lib/http_endpoint/apiv1/command.ex
+++ lib/http_endpoint/apiv1/command.ex
@@ -2,28 +2,62 @@ defmodule HttpEndpoint.Apiv1.Command do
   require Logger
   use Plug.Router
 
-  plug :match
-  plug Plug.Parsers, parsers: [:json], json_decoder: Poison
-  plug :dispatch
+  plug(:match)
+  plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
+  plug(:dispatch)
 
   post "/forcestate" do
     id = conn.params["id"]
 
-    case conn.body_params do
-      %{"state" => state, "time" => time} ->
-        true = ["idle", "clean", "fill"] |> Enum.member?(state)
-        time = String.to_integer(time)
-        command = %{
-          command_type: :force_state,
-          payload: %{
-            state: state,
-            time: time
-          }
-        }
-        DeviceRegistry.set_command(id, command)
-        send_resp(conn, 200, "")
-      _ ->
-        send_resp(conn, 400, "Bad request")
-      end
+    # verify that state and time are present and valid
+    %{"state" => state, "time" => time} = conn.body_params
+    true = ["idle", "clean", "fill"] |> Enum.member?(state)
+    true = is_integer(time)
+
+    command = %{
+      command_type: :force_state,
+      payload: %{
+        state: state,
+        time: time
+      }
+    }
+
+    DeviceRegistry.set_command(id, command)
+    send_resp(conn, 200, "")
+  end
+
+  post "/setconfig" do
+    id = conn.params["id"]
+
+    %{
+      "waterlevel_fill_start" => water_fill_start,
+      "waterlevel_fill_end" => water_fill_end,
+      "clean_before_fill_duration" => clean_before_fill_duration,
+      "clean_after_fill_duration" => clean_after_fill_duration,
+      "leak_protection" => leak_protection
+    } = conn.body_params
+
+    true = is_integer(water_fill_start)
+    true = is_integer(water_fill_end)
+    true = is_integer(clean_before_fill_duration)
+    true = is_integer(clean_after_fill_duration)
+    true = is_boolean(leak_protection)
+
+    config = %{
+      waterlevel_fill_start: water_fill_start,
+      waterlevel_fill_end: water_fill_end,
+      clean_before_fill_duration: clean_before_fill_duration,
+      clean_after_fill_duration: clean_after_fill_duration,
+      leak_protection: leak_protection
+    }
+
+    command = %{
+      command_type: :update_config,
+      payload: config
+    }
+
+    DeviceRegistry.set_command(id, command)
+
+    send_resp(conn, 200, "")
   end
 end
